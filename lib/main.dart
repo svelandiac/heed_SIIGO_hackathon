63a61fbb8ad2d83e98c7e9c4e610f98c07e608fd
svelandiac <svelandiacasas@gmail.com>
2020-02-08 11:31:37 -0500

    Importing plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HeedApp());
 
-class MyApp extends StatelessWidget {
+class HeedApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
